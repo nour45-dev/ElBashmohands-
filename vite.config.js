67066dba41974a39c79c61509a15a5ae49fd4cc5
nour45-dev <noureldinahmedh@gmail.com>
2026-08-10 09:56:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,12 @@ export default defineConfig({
   ],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
   }
 })
